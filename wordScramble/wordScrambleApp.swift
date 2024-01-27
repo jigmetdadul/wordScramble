8e94d4a3a9518e91fe22787a2b0170d853da92a6
Jigmet Dadul <85403349+jigmetdadul@users.noreply.github.com>
2024-01-27 21:14:32 +0530

    Initial Commit

---
+++ wordScramble/wordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  wordScrambleApp.swift
+//  wordScramble
+//
+//  Created by Jigmet stanzin Dadul on 27/01/24.
+//
+
+import SwiftUI
+
+@main
+struct wordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
